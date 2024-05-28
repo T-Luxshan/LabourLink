b332381b9af04dd150f02394d779833d534b2f5d
T-Luxshan <luckybraveboys@gmail.com>
2024-05-29 00:41:08 +0530

    Dialaog box added and some styles modified

--- src/screens/authentication/ChangePassword.js
+++ src/screens/authentication/ChangePassword.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Image, Text, TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
-import { TextInput, Button } from 'react-native-paper';
+import { TextInput, Button, Dialog, Portal, PaperProvider } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import * as yup from 'yup';
 import PasswordModel from '../../components/PasswordModel';
@@ -12,7 +12,8 @@ const ChangePassword = () => {
   const [rightIcon, setRightIcon] = useState('eye-slash'); 
   const [errors, setErrors] = useState({});
   const [mState, setMState] = useState(false);
-  
+  const [dialogVisible, setDialogVisible] = useState(false);
+
   const theme = {
     colors: {
       primary: 'white', 
@@ -20,6 +21,11 @@ const ChangePassword = () => {
   };
 
   const handleModel = (mState) => setMState(mState);
+  
+  const hideDialogBox = () => {
+    setDialogVisible(false) 
+    setMState(false);
+  }
 
   const schema = yup.object().shape({
     password: yup
@@ -44,6 +50,8 @@ const ChangePassword = () => {
     try {
       await schema.validate({ password, confirmPassword }, { abortEarly: false });
       setErrors({});
+      setDialogVisible(true);
+      setMState(true);
     } catch (error) {
       const validationErrors = {};
       error.inner.forEach(err => {
@@ -54,90 +62,107 @@ const ChangePassword = () => {
   };
 
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior={Platform.OS === "ios" ? "padding" : null}
-      keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
-    >
-      <ScrollView contentContainerStyle={[styles.forgotPasswordContainer, mState && { backgroundColor: 'rgba(0, 0, 0, 0.5)' }]}>
-        <Image
-          source={require('../../assets/Images/Ellipse3.png')}
-          resizeMode="contain"
-          style={styles.topImage}
-        />
-        <View style={styles.foreground}>
-        <PasswordModel onMStateChange={handleModel} marginTop={250} />
-          <View style={styles.inputContainer}>
-            <View style={styles.password}>
-              
-              <TextInput
-                theme={theme}
-                underlineColor="transparent"
-                placeholder="Password"
-                autoCapitalize="none"
-                autoCorrect={false}
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry={passwordVisibility}
-                style={[styles.input, mState && { backgroundColor: '#797979' }]}
-              />
-              <TouchableOpacity
-                style={styles.iconButton}
-                onPress={handlePasswordVisibility}
-              >
-                <Icon name={rightIcon} size={20} color="black" />
-              </TouchableOpacity>
+    // <PaperProvider>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : null}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+      >
+        <ScrollView contentContainerStyle={[styles.forgotPasswordContainer, mState && { backgroundColor: 'rgba(0, 0, 0, 0.5)' }]}>
+          <Image
+            source={require('../../assets/Images/Ellipse3.png')}
+            resizeMode="contain"
+            style={styles.topImage}
+          />
+          <View style={styles.foreground}>
+            <PasswordModel onMStateChange={handleModel} marginTop={250} Password={"Change Password"} />
+            <View style={styles.inputContainer}>
+              <View style={styles.password}>
+                <TextInput
+                  theme={theme}
+                  underlineColor="transparent"
+                  selectionColor="black"
+                  placeholder="Password"
+                  placeholderTextColor="#797979"  // Grey color for placeholder text
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={passwordVisibility}
+                  style={[styles.input, mState && { backgroundColor: '#797979'} ]} 
+                />
+                <TouchableOpacity
+                  style={styles.iconButton}
+                  onPress={handlePasswordVisibility}
+                >
+                  <Icon name={rightIcon} size={20} color="black" />
+                </TouchableOpacity>
+              </View>
+              {errors.password && <Text style={styles.error}>{errors.password}</Text>}
             </View>
-            {errors.password && <Text style={styles.error}>{errors.password}</Text>}
-          </View>
-          
-          <View style={styles.inputContainer}>
-            <View style={styles.password}>
-              <TextInput
-                theme={theme}
-                underlineColor="transparent"
-                placeholder="Confirm password"
-                autoCapitalize="none"
-                autoCorrect={false}
-                value={confirmPassword}
-                onChangeText={setConfirmPassword}
-                secureTextEntry={passwordVisibility}
-                style={[styles.input, mState && { backgroundColor: '#797979' }]}
-              />
-              <TouchableOpacity
-                style={styles.iconButton}
-                onPress={handlePasswordVisibility}
-              >
-                <Icon name={rightIcon} size={20} color="black" />
-              </TouchableOpacity>
+
+            <View style={styles.inputContainer}>
+              <View style={styles.password}>
+                <TextInput
+                  theme={theme}
+                  underlineColor="transparent"
+                  placeholder="Confirm password"
+                  placeholderTextColor="#797979"  // Grey color for placeholder text
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  value={confirmPassword}
+                  onChangeText={setConfirmPassword}
+                  secureTextEntry={passwordVisibility}
+                  style={[styles.input, mState && { backgroundColor: '#797979'} ]} 
+                />
+                <TouchableOpacity
+                  style={styles.iconButton}
+                  onPress={handlePasswordVisibility}
+                >
+                  <Icon name={rightIcon} size={20} color="black" />
+                </TouchableOpacity>
+              </View>
+              {errors.confirmPassword && <Text style={styles.error}>{errors.confirmPassword}</Text>}
             </View>
-            {errors.confirmPassword && <Text style={styles.error}>{errors.confirmPassword}</Text>}
+
+            {!mState && (
+              <Button
+                mode="contained"
+                buttonColor={mState ? '#6D6D6D' : "#FB9741"}
+                textColor={mState ? '#797979' : "white"}
+                onPress={handleOTP}
+                style={styles.btn}
+                labelStyle={styles.buttonText}
+              >
+                Change Password
+              </Button>
+            )}
           </View>
-          
-          { !mState && <Button 
-            mode="contained" 
-            buttonColor={mState ? '#6D6D6D' : "#FB9741"} 
-            textColor={mState ? '#797979' : "white"}  
-            onPress={handleOTP} 
-            style={styles.btn}  
-            labelStyle={styles.buttonText}
-          >
-            Change Password
-          </Button>}
-        </View>
-        
-        <Image
 
-        //   source= {mState ? require('../../assets/Images/Reset2.png') : require('../../assets/Images/Reset.png') } 
-          source= {require('../../assets/Images/Reset.png') } 
-          resizeMode="contain"
-          style={[styles.bottomImage, mState && { opacity: 0.5 }]}
-
-        />
-      </ScrollView>
-    </KeyboardAvoidingView>
+          <Image
+            source={require('../../assets/Images/Reset.png')}
+            resizeMode="contain"
+            style={[styles.bottomImage, mState && { opacity: 0.5 }]}
+          />
+        <PaperProvider>
+          <Portal>
+            <Dialog visible={dialogVisible} onDismiss={() => setDialogVisible(false)}
+            style={styles.DialogBox}>
+              <Dialog.Title style={{color:'green', fontSize: 30, fontWeight:'bold',  marginTop: 30}}>Done :)</Dialog.Title>
+              <Dialog.Content >
+                <Text style={{ fontSize: 17, fontWeight:700, marginTop: 15, marginBottom:-10}}>Now you will be redirected to log in.</Text>
+              </Dialog.Content>
+              <Dialog.Actions>
+                <Button labelStyle={{fontSize:21, fontWeight:'bold'}} textColor='#FB9741' onPress={hideDialogBox}>OK</Button>
+              </Dialog.Actions>
+            </Dialog>
+          </Portal>
+          </PaperProvider>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    // </PaperProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -149,17 +174,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   topImage: {
-    width: '100%', 
+    width: '100%',
     height: 200,
-    position: 'absolute', 
-    top: -45, 
-    left: 0,  
+    position: 'absolute',
+    top: -45,
+    left: 0,
     zIndex: 0,
     transform: [{ rotate: '-2deg' }]
   },
   foreground: {
     width: '80%',
-    marginBottom: 120,
+    marginTop: 250,
     zIndex: 1,
   },
   inputContainer: {
@@ -169,6 +194,7 @@ const styles = StyleSheet.create({
   input: {
     marginTop: 10,
     backgroundColor: '#F2F4F7',
+    color: 'red',
     borderRadius: 10,
     zIndex: 3,
   },
@@ -184,11 +210,11 @@ const styles = StyleSheet.create({
     zIndex: -1,
   },
   bottomImage: {
-    width: 500, 
+    width: 500,
     height: 500,
-    position: 'absolute', 
-    top: 400, 
-    left:0, 
+    position: 'absolute',
+    top: 400,
+    left: 0,
     zIndex: 0,
   },
   password: {
@@ -204,6 +230,16 @@ const styles = StyleSheet.create({
     color: 'red',
     marginTop: 5,
   },
+  DialogBox:{
+    backgroundColor:'white',
+    position: 'absolute',
+    right: -200,
+    top: 15,
+    // zIndex: 999,
+    // height:180,
+    width:350,
+    zIndex: 800,
+  }
 });
 
 export default ChangePassword;
