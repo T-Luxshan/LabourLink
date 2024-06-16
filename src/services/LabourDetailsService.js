6a853d315f6259600c9bb89aa23a6292918ab3fd
Thanakarn <rasenthiramthana2399@gmail.com>
2024-06-16 14:26:24 +0530

    LabourDetailsService.js file added

---
+++ src/services/LabourDetailsService.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const BASE_URL = "http://192.168.1.207:8080/api/labour";
+const REVIEW_BASE_URL = "http://192.168.1.207:8080/api/v1/labourReview";
+const ABOUT_BASE_URL = "http://192.168.1.207:8080/api/labour-profiles";
+const TATAL_SERVICE_URL ="http://192.168.1.207:8080/api/bookings"
+
+
+export const getLabourByEmail = (email) => {
+    return axios.get(`${BASE_URL}/getLabourById/${email}`)
+}
+
+export const getLabourByReview = (email, jobRole) => {
+     return axios.get(`${REVIEW_BASE_URL}/getReviews/${email}/${jobRole}`)
+ }
+ 
+export const getLabourByRating = (email) => {
+    return axios.get(`${REVIEW_BASE_URL}/rating/${email}`)
+}
+
+
+ export const getLabourByAbout = (email) => {
+    return axios.get(`${ABOUT_BASE_URL}/getLabourProfileById/${email}`)
+} 
+
+export const getLabourByTotalservice = (labourEmail, stage) => {
+    return axios.get(`${REVIEW_BASE_URL}/getReviews/${labourEmail}/${stage}`)
+}
