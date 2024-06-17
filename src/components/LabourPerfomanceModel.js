65bc3a833537826e5dff5354b3526d39a3ec4181
T-Luxshan <luckybraveboys@gmail.com>
2024-06-17 17:32:54 +0530

    model style modified

--- src/components/LabourPerfomanceModel.js
+++ src/components/LabourPerfomanceModel.js
@@ -4,48 +4,21 @@ import { Button, Modal, Portal, Provider as PaperProvider, Headline, IconButton 
 
 const LabourPerfomanceModel = ({onMStateChange, marginTop, Password}) => {
   const [visible, setVisible] = React.useState(false);
-  const showModal = () => {
-    setVisible(true);
-    onMStateChange(true);
-  };
-  const hideModal = () => {
-    setVisible(false);
-    onMStateChange(false);
-  };
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = {backgroundColor: 'white', padding: 20};
 
   return (
     <PaperProvider>
       <Portal>
-         <Modal visible={visible} onDismiss={hideModal}
-          overlayOpacity={0} 
-                contentContainerStyle={[styles.modelContainer, {marginTop: marginTop,}]} >
-          <Headline style={styles.headline}>Perfomance summery</Headline>
-          <Text style={styles.modelText}>
-           Please follow these rules to create your password{'\n\n'}
-           Your password must,
-            {'\n\n'}• At least 5 characters long.
-            {'\n\n'}• Contain at least one uppercase letter
-            {'\n\n'}• Contain at least one lowercase letter
-            {'\n\n'}• Contain at least one number.
-            {'\n\n'}
-          </Text>
-          <Button mode="outlined" textColor="#F97300" onPress={hideModal} style={{ borderColor: '#F97300' }} >
-            Dismiss
-          </Button>
+        <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={styles.modelContainer}>
+          <Text>Example Modal.  Click outside this area to dismiss.</Text>
         </Modal>
       </Portal>
-      <View style={styles.uploadContainer}>
-        <View style={styles.infoContainer}>
-          <Text style={styles.promptText}>{Password}</Text>
-          <IconButton
-            icon="information-outline"
-            size={18}
-            onPress={showModal}
-            iconColor="red" 
-            style={styles.infoIcon}
-          />
-        </View>
-      </View>
+      <Button style={{marginTop: 30}} onPress={showModal}>
+        Show
+      </Button>
     </PaperProvider>
   );
 };
@@ -54,36 +27,39 @@ export default LabourPerfomanceModel;
 
 const styles = StyleSheet.create({
   modelContainer: {
+    // flex:1,
     backgroundColor: 'white',
-    padding: 20,
-    height:350,
-    width: '100%',
-    marginTop: 10,
+    // padding: 90,
+    height:500,
+    // width: '100%',
+    margin: 20,
     borderRadius: 10,
-    zIndex: 9999
-  },
-  headline: {
-    color: 'black',
-    marginBottom: 10,
-  },
-  modelText: {
-    color: 'black',
-  },
-  infoContainer: {
-    flexDirection: 'row',
+    // zIndex: 9999
     alignItems: 'center',
+    justifyContent: 'center',
   },
-  infoIcon: {
-    marginLeft: 5,
-    elevation: 0, // for Android
-    shadowOpacity: 0, // for iOS
-  },
-  promptText: {
-    color: 'black',
-  },
-  uploadContainer: {
-    marginTop: -15,
-    marginBottom: -15,
-  },
+  // headline: {
+  //   color: 'black',
+  //   marginBottom: 10,
+  // },
+  // modelText: {
+  //   color: 'black',
+  // },
+  // infoContainer: {
+  //   flexDirection: 'row',
+  //   alignItems: 'center',
+  // },
+  // infoIcon: {
+  //   margin: 150,
+  //   elevation: 0, // for Android
+  //   shadowOpacity: 0, // for iOS
+  // },
+  // promptText: {
+  //   color: 'black',
+  // },
+  // uploadContainer: {
+  //   marginTop: -15,
+  //   // marginBottom: -15,
+  // },
 });
 
