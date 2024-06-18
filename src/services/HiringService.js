8dc72f34cd2ea8c67a9df086b364c14aabfc6470
T-Luxshan <luckybraveboys@gmail.com>
2024-06-19 00:09:38 +0530

    axios to axiosAuthInstance

--- src/services/HiringService.js
+++ src/services/HiringService.js
@@ -4,5 +4,5 @@ import axios from "axios";
 const REST_API_BASE_URL_BOOKING = "http://192.168.1.56:8080/api/bookings";
 
 export const getBookingDetailsByLabourId = (email) => {
-    return axios.get(`${REST_API_BASE_URL_BOOKING}/labour/${email}`)
+    return axiosAuthInstance.get(`${REST_API_BASE_URL_BOOKING}/labour/${email}`)
 }
--- src/services/ReviewService.js
+++ src/services/ReviewService.js
@@ -20,5 +20,5 @@ export const editReview = (id, jobRole, description, rating, labourEmail) => {
 };
 
 export const getReviewById = (email) => {
-    return axios.get(`${REST_API_BASE_URL_REVIEW}/getReviewById/${email}`)
+    return axiosAuthInstance.get(`${REST_API_BASE_URL_REVIEW}/getReviewById/${email}`)
 } 
