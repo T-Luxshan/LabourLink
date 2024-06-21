2a657e571b3f37a6b26eb60e51f51ee98e712847
Thanakarn <rasenthiramthana2399@gmail.com>
2024-06-21 14:31:38 +0530

    LabourDetailsService.js

--- src/services/LabourDetailsService.js
+++ src/services/LabourDetailsService.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const BASE_URL = "http://192.168.1.207:8080/api/labour";
 const REVIEW_BASE_URL = "http://192.168.1.207:8080/api/v1/labourReview";
 const ABOUT_BASE_URL = "http://192.168.1.207:8080/api/labour-profiles";
-const TATAL_SERVICE_URL ="http://192.168.1.207:8080/api/bookings"
-
-
+const TATAL_SERVICE_URL ="http://192.168.1.207:8080/api/bookings";
+const LABOUR_LOCATION_URL ="http://192.168.1.207:8080/api/labour-locations";
+// const LABOUR_DETAILS_URL ="http://172.20.10.4:8080/api/labour";192.168.1.207 172.20.10.4
 
 export const getLabourByEmail = (email) => {
     return axios.get(`${BASE_URL}/getLabourById/${email}`)
@@ -33,6 +33,13 @@ export const BookingLabour = (labourId,customerId,date,startTime, bookingStage,j
         labourId,customerId,date,startTime, bookingStage,jobDescription,jobRole
     });
   };
+
+  export const getLocationsByJobRole = (jobRole) =>{
+return axios.get(`${LABOUR_LOCATION_URL }/labours/${jobRole}/locations`)
+}
  
+export const getLabourByJobRole = (jobRole) =>{
+    return axios.get(`${BASE_URL}/getLabourByJobRole/${jobRole}`)
+    }
 
  
