8eed9ac044555f70aea31969c71549ffee7dbb84
T-Luxshan <luckybraveboys@gmail.com>
2024-06-26 16:33:13 +0530

    my IP address added to connect with backend

--- src/services/LabourProfileService.js
+++ src/services/LabourProfileService.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 // Base URL for your API
 const BASE_URL = "http://172.20.10.3:8080/api/labour-profiles";
+// const BASE_URL = "http://192.168.1.56:8080/api/labour-profiles";
+
 
 
 // export const getLabourProfileById = async (labourEmail) => {
@@ -42,19 +44,22 @@ const BASE_URL = "http://172.20.10.3:8080/api/labour-profiles";
 
 
 
-export const getLabourProfileById = async (labourEmail) => {
-  try {
-    console.log(`Fetching Labour Profile for: ${labourEmail}`);
-    const response = await axios.get(
-      `${BASE_URL}/getLabourProfileById/${labourEmail}`
-    );
-    console.log("Labour Profile Response:", response.data);
-    return response.data; // Ensure this is correctly returning the data
-  } catch (error) {
-    console.error("Error fetching Labour Profile:", error);
-    throw error;
+// export const getLabourProfileById = async (labourEmail) => {
+//   try {
+//     console.log(`Fetching Labour Profile for: ${labourEmail}`);
+//     const response = await axios.get(
+//       `${BASE_URL}/getLabourProfileById/${labourEmail}`
+//     );
+//     console.log("Labour Profile Response:", response.data);
+//     return response.data; // Ensure this is correctly returning the data
+//   } catch (error) {
+//     console.error("Error fetching Labour Profile:", error);
+//     throw error;
+//   }
+// };
+export const getLabourProfileById = (labourEmail) => {
+  return axios.get(`${BASE_URL}/getLabourProfileById/${labourEmail}`);
   }
-};
 
 // API for updating the profile of a specific labour
 // export const updateLabourProfile = async (
