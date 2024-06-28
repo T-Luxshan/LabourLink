0b650e04e9e919f32c16feac195265e5dae4c916
T-Luxshan <luckybraveboys@gmail.com>
2024-06-28 22:56:53 +0530

    unwanted space

--- src/screens/Work_History.js
+++ src/screens/Work_History.js
@@ -5,7 +5,6 @@ import ReviewModel from "../components/ReviewModel";
 import { getBooingDetailsById } from "../services/CustomerBookingService";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
 const Work_History = ({ route, navigation }) => {
   const { completedBookings } = route.params;
   // const email = "aruran@example.com";
