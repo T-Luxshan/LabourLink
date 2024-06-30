905ba2a796be64b59379bf97eb390d28b852124b
Kirushanth <kirushan06@gmail.com>
2024-06-30 18:51:45 +0530

    before pull

--- src/screens/BookAppointment.js
+++ src/screens/BookAppointment.js
@@ -1,55 +1,66 @@
-import React, { useState , useEffect} from 'react'; 
-import { View, Text, StyleSheet, TouchableOpacity, Platform, TextInput, KeyboardAvoidingView, Alert } from 'react-native'; 
-import { Calendar } from 'react-native-calendars'; 
-import LabourProfileComponent from '../components/LabourProfileComponent'; 
-import AppBar from '../components/AppBar';  
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { ScrollView } from 'react-native-gesture-handler';
-import { Formik } from 'formik';  
-import * as Yup from 'yup';  
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { BookingLabour } from '../services/LabourDetailsService';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  TextInput,
+  KeyboardAvoidingView,
+  Alert,
+} from "react-native";
+import { Calendar } from "react-native-calendars";
+import LabourProfileComponent from "../components/LabourProfileComponent";
+import AppBar from "../components/AppBar";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { ScrollView } from "react-native-gesture-handler";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { BookingLabour } from "../services/LabourDetailsService";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { saveNotifications } from '../services/NoificationSevice';
+import { saveNotifications } from "../services/NoificationSevice";
 
 // Define Yup validation schema
 const bookingSchema = Yup.object().shape({
   date: Yup.string()
-    .required('Date is required')
-    .matches(
-      /^\d{4}-\d{2}-\d{2}$/,
-      'Date must be in the format YYYY-MM-DD'
-    ),
+    .required("Date is required")
+    .matches(/^\d{4}-\d{2}-\d{2}$/, "Date must be in the format YYYY-MM-DD"),
   startTime: Yup.string()
-    .required('Start time is required')
+    .required("Start time is required")
     .matches(
       /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/,
-      'Start time must be in the format HH:MM'
+      "Start time must be in the format HH:MM"
     ),
-  jobDescription: Yup.string()
-    .required('Job description is required')
-    // .min(10, 'Job description must be at least 10 characters long')
+  jobDescription: Yup.string().required("Job description is required"),
+  // .min(10, 'Job description must be at least 10 characters long')
 });
 
 const BookAppointment = () => {
   const navigation = useNavigation();
   const route = useRoute();
-  const { labourId, jobRole, labourName, labourJobTitle, labourRating, profileImage } = route.params;
+  const {
+    labourId,
+    jobRole,
+    labourName,
+    labourJobTitle,
+    labourRating,
+    profileImage,
+  } = route.params;
 
-
-  const [selectedDate, setSelectedDate] = useState('');
+  const [selectedDate, setSelectedDate] = useState("");
   const [selectedTime, setSelectedTime] = useState(new Date());
   const [showTimePicker, setShowTimePicker] = useState(false);
   const [timePicked, setTimePicked] = useState(false);
-  const tempProfile = "https://firebasestorage.googleapis.com/v0/b/labourlink-e7ecf.appspot.com/o/ProfilePhoto%2Fboy.png?alt=media&token=b9013246-c51f-4bb8-b68b-1465e24e8583"
-  const[customerId, setCustomerId] = useState(""); 
-  const[customerName, setCustomerName] = useState(""); 
-  
+  const tempProfile =
+    "https://firebasestorage.googleapis.com/v0/b/labourlink-e7ecf.appspot.com/o/ProfilePhoto%2Fboy.png?alt=media&token=b9013246-c51f-4bb8-b68b-1465e24e8583";
+  const [customerId, setCustomerId] = useState("");
+  const [customerName, setCustomerName] = useState("");
+
   // const labourId = "thana@example.com";
   // const customerId = "aruran@example.com";
   const bookingStage = "PENDING";
   // const jobRole = "ELECTRICIAN";
-
 
   useEffect(() => {
     const fetchEmail = async () => {
@@ -78,7 +89,7 @@ const BookAppointment = () => {
 
   const handleTimeChange = (event, selectedDate) => {
     const currentDate = selectedDate || selectedTime;
-    setShowTimePicker(Platform.OS === 'ios');
+    setShowTimePicker(Platform.OS === "ios");
     setSelectedTime(currentDate);
     setTimePicked(true);
   };
@@ -98,19 +109,27 @@ const BookAppointment = () => {
     //   jobRole
     // };
 
-    console.log("Submitting booking data: ",  values); // Log the booking data
-    console.log(jobRole.toUpperCase(), labourId)
-    console.log(customerId)
+    console.log("Submitting booking data: ", values); // Log the booking data
+    console.log(jobRole.toUpperCase(), labourId);
+    console.log(customerId);
 
     try {
-      const response = await BookingLabour( labourId,customerId,values.date, values.startTime, "PENDING", values.jobDescription,jobRole.toUpperCase());
-      Alert.alert('Success', 'Booking has been made successfully.');
+      const response = await BookingLabour(
+        labourId,
+        customerId,
+        values.date,
+        values.startTime,
+        "PENDING",
+        values.jobDescription,
+        jobRole.toUpperCase()
+      );
+      Alert.alert("Success", "Booking has been made successfully.");
       console.log("Booking response: ", response.data);
       HiredNotificationToCustomer();
       HiredNotificationToLabour();
-      resetForm();  // Reset the form after successful submission
+      resetForm(); // Reset the form after successful submission
     } catch (error) {
-      Alert.alert('Error', 'Failed to make the booking. Please try again.');
+      Alert.alert("Error", "Failed to make the booking. Please try again.");
       console.error("Booking error: ", error);
     }
   };
@@ -123,19 +142,19 @@ const BookAppointment = () => {
       createdAt: new Date().toISOString(),
     };
 
-    await fetch('https://app.nativenotify.com/api/indie/notification', {
-      method: 'POST',
+    await fetch("https://app.nativenotify.com/api/indie/notification", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer dwb6dAoCmrQD8faaLyciTU`,
       },
       body: JSON.stringify({
         appId: 21639,
-        appToken: 'dwb6dAoCmrQD8faaLyciTU',
+        appToken: "dwb6dAoCmrQD8faaLyciTU",
         title: notification.title,
         message: notification.message,
         // userId: notification.recipient,
-        subID:customerId,
+        subID: customerId,
         date: notification.createdAt,
       }),
     });
@@ -143,7 +162,7 @@ const BookAppointment = () => {
     try {
       await saveNotifications(notification);
     } catch (error) {
-      console.error('Error saving notification', error);
+      console.error("Error saving notification", error);
     }
   };
 
@@ -155,19 +174,19 @@ const BookAppointment = () => {
       createdAt: new Date().toISOString(),
     };
 
-    await fetch('https://app.nativenotify.com/api/indie/notification', {
-      method: 'POST',
+    await fetch("https://app.nativenotify.com/api/indie/notification", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer dwb6dAoCmrQD8faaLyciTU`,
       },
       body: JSON.stringify({
         appId: 21639,
-        appToken: 'dwb6dAoCmrQD8faaLyciTU',
+        appToken: "dwb6dAoCmrQD8faaLyciTU",
         title: notification.title,
         message: notification.message,
         // userId: notification.recipient,
-        subID:labourId,
+        subID: labourId,
         date: notification.createdAt,
       }),
     });
@@ -175,38 +194,51 @@ const BookAppointment = () => {
     try {
       await saveNotifications(notification);
     } catch (error) {
-      console.error('Error saving notification', error);
+      console.error("Error saving notification", error);
     }
   };
 
   return (
     <Formik
-      initialValues={{ date: '', startTime: '', jobDescription: '' }}
+      initialValues={{ date: "", startTime: "", jobDescription: "" }}
       validationSchema={bookingSchema}
       onSubmit={handleSubmit}
     >
-      {({ handleChange, handleBlur, handleSubmit, values, errors, touched, setFieldValue }) => (
-        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        touched,
+        setFieldValue,
+      }) => (
+        <KeyboardAvoidingView
+          style={styles.container}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
           <ScrollView style={styles.container}>
-            <AppBar Title="BookAppointment" /> 
+            <AppBar Title="BookAppointment" />
 
             <View style={styles.Labourprofile}>
-              <LabourProfileComponent 
-                profileImage={{uri:profileImage ? profileImage : tempProfile  }}
+              <LabourProfileComponent
+                profileImage={{
+                  uri: profileImage ? profileImage : tempProfile,
+                }}
                 name={labourName}
-                jobTitle= {labourJobTitle}
-                rating={labourRating} 
-              /> 
+                jobTitle={labourJobTitle}
+                rating={labourRating}
+              />
             </View>
 
             <Text style={styles.label}>Select Date:</Text>
             <Calendar
               onDayPress={(day) => {
                 handleDateSelect(day);
-                setFieldValue('date', day.dateString);  // Set Formik field value
+                setFieldValue("date", day.dateString); // Set Formik field value
               }}
               markedDates={{
-                [selectedDate]: { selected: true, selectedColor: 'blue' },
+                [selectedDate]: { selected: true, selectedColor: "blue" },
               }}
             />
             {/* {selectedDate && (
@@ -214,30 +246,31 @@ const BookAppointment = () => {
                  Selected Date: {selectedDate} 
               </Text>
             )} */}
-<View style={styles.Time}>
-             <TouchableOpacity onPress={showTimePickerModal}>
-              
-              <Text style={styles.label2}>Select Time:</Text>
-             </TouchableOpacity>
-            {showTimePicker && (
-              <DateTimePicker
-                style={styles.Timepicker}
-                value={selectedTime}
-                mode="time"
-                is24Hour={false}
-                // display="default"
-                 onChange={(event, date) => {
-                  handleTimeChange(event, date);
-                   setFieldValue('startTime', date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));  // Set Formik field value
-                  
-                }}
-              />
-            
-              
-            )}
+            <View style={styles.Time}>
+              <TouchableOpacity onPress={showTimePickerModal}>
+                <Text style={styles.label2}>Select Time:</Text>
+              </TouchableOpacity>
+              {showTimePicker && (
+                <DateTimePicker
+                  style={styles.Timepicker}
+                  value={selectedTime}
+                  mode="time"
+                  is24Hour={false}
+                  // display="default"
+                  onChange={(event, date) => {
+                    handleTimeChange(event, date);
+                    setFieldValue(
+                      "startTime",
+                      date.toLocaleTimeString([], {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })
+                    ); // Set Formik field value
+                  }}
+                />
+              )}
             </View>
-              
-            
+
             {/* {timePicked && (
               <Text style={styles.selectedTimeText}>
                 Selected Time: {selectedTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
@@ -247,22 +280,24 @@ const BookAppointment = () => {
             <View style={styles.jobDetailsContainer}>
               <Text style={styles.jobDetailsHeader}>Job Detail :</Text>
               <View style={styles.jobDetailRow}>
-                <TextInput 
+                <TextInput
                   style={styles.jobDescriptionInput}
                   placeholder="Type job description here..."
                   multiline
                   value={values.jobDescription}
-                  onChangeText={handleChange('jobDescription')}
-                  onBlur={handleBlur('jobDescription')}
+                  onChangeText={handleChange("jobDescription")}
+                  onBlur={handleBlur("jobDescription")}
                 />
-                
               </View>
               {errors.jobDescription && touched.jobDescription && (
-                  <Text style={styles.errorText}>{errors.jobDescription}</Text>
-                )}
+                <Text style={styles.errorText}>{errors.jobDescription}</Text>
+              )}
             </View>
 
-            <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+            <TouchableOpacity
+              style={styles.submitButton}
+              onPress={handleSubmit}
+            >
               <Text style={styles.submitButtonText}>Booking</Text>
             </TouchableOpacity>
           </ScrollView>
@@ -279,7 +314,7 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 10,
     marginTop: 0,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   label: {
     fontSize: 18,
@@ -289,7 +324,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginTop: 10,
     marginBottom: 15,
-    color: 'blue',
+    color: "blue",
   },
   Labourprofile: {
     marginBottom: 20,
@@ -302,34 +337,34 @@ const styles = StyleSheet.create({
   },
   jobDetailsHeader: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   jobDetailRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   jobDescriptionInput: {
     flex: 1,
     height: 100,
-    borderColor: '#ddd',
+    borderColor: "#ddd",
     borderWidth: 1,
     borderRadius: 5,
     padding: 10,
-    textAlignVertical: 'top',
-    backgroundColor: '#fff',
+    textAlignVertical: "top",
+    backgroundColor: "#fff",
   },
   submitButton: {
-    backgroundColor: 'orange',
+    backgroundColor: "orange",
     padding: 15,
     borderRadius: 50,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 20,
   },
   submitButtonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   selectedTimeText: {
     fontSize: 18,
@@ -340,15 +375,14 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   errorText: {
-    color: 'red',
+    color: "red",
     fontSize: 14,
     marginTop: 5,
   },
-  Timepicker:{
-    marginRight:170,
-  
+  Timepicker: {
+    marginRight: 170,
   },
-  Time:{
-    flexDirection:"row"
-  }
+  Time: {
+    flexDirection: "row",
+  },
 });
