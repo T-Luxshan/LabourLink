c459d7d74f000fac4a111137154b726ab3b73078
Kirushanth <kirushan06@gmail.com>
2024-06-30 19:13:53 +0530

    Click to notify button removed

--- src/screens/Notification.js
+++ src/screens/Notification.js
@@ -188,7 +188,7 @@ const Notification = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
-        <Button title="Click to Notify" onPress={handleNotification} />
+        {/* <Button title="Click to Notify" onPress={handleNotification} /> */}
         <Text style={styles.heading}>Notifications</Text>
       </View>
       <ScrollView style={styles.scrollContainer} contentContainerStyle={styles.scrollContent}>
